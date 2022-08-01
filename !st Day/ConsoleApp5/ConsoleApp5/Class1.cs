5041f50793d50749564cb73ede276b69698a2073
Adarsh <adarsh.singh@venture7.com>
2022-08-01 15:43:00 +0530

    1st day

---
+++ !st Day/ConsoleApp5/ConsoleApp5/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Class1
+    {
+         static void Main()
+        {
+            Console.WriteLine("Second Main");
+        }
+    }
+}
